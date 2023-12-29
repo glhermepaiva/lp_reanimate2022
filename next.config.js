75652d5a991294d08cdd84584b6499ecda611db2
unknown <guilherme@pointmedia.com.br>
2023-12-29 19:09:17 -0300

    Novo layout up1

--- next.config.js
+++ next.config.js
@@ -1,15 +1,10 @@
-module.exports = {
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      '/': { page: '/' },
-      '/formen': { page: '/formen' },
-      '/formbr': { page: '/formbr' },
-      '/formcompleteen': { page: '/formcompleteen' },
-      '/formcompletebr': { page: '/formcompletebr' },
-    }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
   },
-  basePath: '',
 }
+
+module.exports = nextConfig
+
